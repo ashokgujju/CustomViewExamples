547422fd23cb62fcc302622bc6e6eeaeb8c0600a
ashok <ashokg.geek@gmail.com>
2017-05-09 11:59:40 +0530

    first commit

---
+++ app/src/main/java/com/ashok/customviewexamples/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ashok.customviewexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
